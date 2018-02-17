71fd9fd4a5bd650ebf80e3df63432aee7867f1af
cquinones100 <cquinones100@gmail.com>
2018-02-17 13:50:38 -0500

    game play constants

---
+++ src/constants/levels.js
@@ -0,0 +1,11 @@
+export const LEVELS = [{
+  id: 1,
+  difficulty: 5,
+  targets: [{ position: 8 }],
+  obstacles: [
+    { position: 3 },
+    { position: 5 },
+    { position: 20 },
+    { position: 25 }
+  ]
+}]
---
+++ src/constants/movementDirections.js
@@ -0,0 +1,4 @@
+export const UP = 38
+export const DOWN = 40
+export const RIGHT = 39
+export const LEFT = 37

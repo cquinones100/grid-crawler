1cedec3009b21e0bb91b5f5326cf04989555b1e5
cquinones100 <cquinones100@gmail.com>
2018-02-19 20:32:52 -0500

    adds support for transition animations

--- src/App.js
+++ src/App.js
@@ -1,21 +1,93 @@
 import React, { Component } from 'react'
 import './App.css'
 import Board from './Board/Board'
+import { LEVELS } from './constants/levels'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { level: 1 }
+
+    this.state = {
+      level: 1,
+      lives: 4,
+      transitioning: false,
+      fadingOut: false
+    }
+  }
+
+  transition(callback) {
+    return this.setState({
+      fadingOut: true
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          fadingOut: false,
+          transitioning: true
+        }, () => {
+          callback(() => {
+            this.setState({
+              transitioning: false,
+              fadingOut: false
+            })
+          })
+        })
+      }, 500)
+    })
+  }
+
+  nextLevel() {
+    const { level } = this.state
+    const nextLevel = level + 1
+
+    if (nextLevel <= LEVELS.length) {
+      this.transition(callback => {
+        this.setState({level: nextLevel}, callback())
+      })
+    }
+  }
+
+  resetLevel() {
+    const { lives } = this.state
+
+    this.transition(callback => {
+      this.setState({
+        lives: lives - 1,
+      }, () => {
+        const { lives } = this.state
+
+        if (lives <= -1) {
+          this.setState({
+            level: 1,
+            lives: 4
+          })
+        }
+      }, callback())
+    })
   }
 
   render() {
-    const { level } = this.state
+    const { level, lives, transitioning, fadingOut } = this.state
 
     if (!level) { return <div /> }
 
+    if (transitioning) { return <div />}
+
+    let className = 'stage-container'
+
+    if (fadingOut) {
+      className += ' fade-out'
+    }
+
     return (
-      <div className='stage-container'>
-        <Board level={1} />
+      <div className={className}>
+        <Board
+          level={level}
+          nextLevel={() => this.nextLevel()}
+          lives={lives}
+          restartLevel={() => this.resetLevel()}
+          transitioning={transitioning}
+          fadingOut={fadingOut}
+        />
       </div>
     )
   }
--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
 import Cell from './Cell'
 import PropTypes from 'prop-types'
-import { LEVELS } from '../constants/levels'
-import { movePlayer } from '../utils/controls/playerControls'
+import { findLevelById } from '../constants/levels'
 import { UP, DOWN, LEFT, RIGHT } from '../constants/movementDirections'
-import { boxPositions } from '../utils/controls/playerControls'
+import {  spacesToMove, move } from '../utils/controls/playerControls'
+import { boxPositions } from '../utils/board/boxPositions'
+import { updateScore } from '../utils/board/updateScore'
+import { targetPositions } from '../utils/board/targetPositions'
+import { totalCells } from '../utils/board/totalCells'
 
 class Board extends Component {
   constructor(props) {
     super(props)
-    const level = LEVELS.find(level => level.id === this.props.level)
+
+    this.state = {
+      level: {},
+      gridArray: [],
+      style: {},
+      totalNumberOfCells: 0,
+      position: 0,
+      score: 0
+    }
+  }
+
+  componentDidMount() {
+    window.document.addEventListener('keydown', e => this.handleKeyDown(e))
+    this.setLevel(this.props.level)
+  }
+
+  setLevel(levelId) {
+    const level = findLevelById(levelId)
     const { difficulty } = level
 
-    const totalNumberOfCells = Math.pow(difficulty, 2)
-
+    const totalNumberOfCells = totalCells(difficulty)
     const gridArray = new Array(totalNumberOfCells).fill('')
     const rowAndColumnArray = new Array(difficulty).fill('')
 
@@ -27,49 +46,97 @@ class Board extends Component {
       height: widthAndHeight + 'px'
     }
 
-    this.state = { level, gridArray, style, totalNumberOfCells, position: 0 }
+    const { startingPosition } = level
+
+    this.setState({
+      ...this.state,
+      level,
+      gridArray,
+      style,
+      totalNumberOfCells,
+      position: startingPosition,
+    })
   }
 
-  componentDidMount() {
-    window.document.addEventListener('keydown', e => this.handleKeyDown(e))
+  componentWillReceiveProps(nextProps) {
+    const { level, lives } = this.props
+    const { level:nextLevel, lives:nextLives, transitioning, fadingOut } = nextProps
+
+    if (transitioning || fadingOut) { return }
+
+    if (level !== nextLevel || lives > nextLives) {
+      return this.setLevel(nextLevel)
+    }
+  }
+
+  componentDidUpdate() {
+    const { score, level } = this.state
+    const { transitioning, fadingOut } = this.props
+
+    if (transitioning || fadingOut) { return }
+
+
+    if (score === targetPositions(level).length) {
+      this.props.nextLevel()
+    }
+  }
+
+  handleMovement(direction) {
+    const { position, level } = this.state
+    const { difficulty, boxes } = level
+    const newPositions = move(position, direction, level)
+
+    const newBoxes = boxPositions(level).map(boxPos => {
+      const box = boxes.find(obs =>
+        obs.position === boxPos
+      )
+
+      if (box.position === newPositions[0]) {
+        return {
+          ...box,
+          position: boxPos + spacesToMove(direction, difficulty)
+        }
+      }
+      return box
+    })
+
+    this.setState({
+      ...this.state,
+      position: newPositions[0],
+      level: {
+        ...this.state.level,
+        boxes: newBoxes
+      }
+    }, () => {
+      const { level, score } = this.state
+      const newScore = updateScore(level)
+
+      if (score !== newScore) {
+        this.setState({
+          ...this.state,
+          score: newScore
+        })
+      }
+    })
   }
 
   handleKeyDown(e) {
     const directions = [UP, DOWN, LEFT, RIGHT]
-    const { position, level } = this.state
-    const { difficulty } = level
     const direction = e.keyCode
+    if (e.keyCode === 82) {
+      this.props.restartLevel()
+    }
 
     if (directions.includes(direction)) {
-      this.setState({
-        position: movePlayer(position, direction, difficulty, level.id)
-      }, () => {
-        const { position, level } = this.state
-        const { obstacles } = level
-
-        if (boxPositions(level).includes(position)) {
-          const newObstacles = obstacles.map(box => {
-            if (parseInt(box.position, 10) === parseInt(position, 10)) {
-              return { ...box, position: box.position + 1 }
-            }
-            return {...box}
-          })
-
-          this.setState({
-            ...this.state,
-            level: {
-              ...this.state.level,
-              obstacles: newObstacles
-            }
-          })
-        }
-      })
+      this.handleMovement(direction)
     }
+
   }
 
   render() {
     const { position, level, style, gridArray } = this.state
-    const { difficulty, obstacles } = level
+    const { difficulty, boxes, targets, spaces } = level
+    const { lives } = this.props
 
     return(
       <div
@@ -82,9 +149,12 @@ class Board extends Component {
             key={index}
             index={index}
             man={index === position ? true : false}
-            obstacle={obstacles.find(obs => obs.position === index) ? true : false}
+            box={boxes.find(box => box.position === index) ? true : false}
             difficulty={difficulty}
             position={position}
+            target={targets.find(targ => targ.position === index) ? true : false}
+            space={spaces.find(space => space.position === index) ? true : false}
+            lives={this.props.lives}
           />
         )}
       </div>

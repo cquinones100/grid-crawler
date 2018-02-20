0563a4213fe93e66bf5a71e595d31d1bed50fdee
cquinones100 <cquinones100@gmail.com>
2018-02-19 20:32:35 -0500

    cell has support for target cells

--- src/Board/Cell.js
+++ src/Board/Cell.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Man from './Man'
 import redBox from './redBox.gif'
+import { calculateDepth } from '../utils/board/calculateDepth'
 
-const Cell = ({ index, man, obstacle, difficulty, position }) => {
+const Cell = ({ index, man, obstacle, difficulty, position, target, box, space, lives }) => {
 
-  const zIndex = index + 1
+  const zIndex = calculateDepth(index, difficulty)
+
+  let backgroundColor = ''
+
+  if (target) { backgroundColor = 'pink' }
+  if (!space) { backgroundColor = 'black' }
 
   return(
-    <div key={index} className='cell' id={index}>
-      {man ? <Man difficulty={difficulty} position={position}/> : ''}
-      {obstacle
+    <div key={index} className='cell' id={index} style={{backgroundColor}}>
+      {man ? <Man difficulty={difficulty} position={position} lives={lives}/> : ''}
+
+      {box
         ? <img className='sprite box' src={redBox} alt='box' style={{zIndex}}/>
         : ''}
     </div>
